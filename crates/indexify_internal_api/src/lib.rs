46a2727c5a7eb9a4f63da400ff30991dbd69f0a4
Zaid Humayun <redixhumayun@gmail.com>
2024-04-26 19:39:57 +0530

    switched root_content_id back to String

--- crates/indexify_internal_api/src/lib.rs
+++ crates/indexify_internal_api/src/lib.rs
@@ -770,7 +770,7 @@ pub type NamespaceName = String;
 pub struct ContentMetadata {
     pub id: ContentMetadataId,
     pub parent_id: ContentMetadataId,
-    pub root_content_id: ContentMetadataId,
+    pub root_content_id: String,
     pub namespace: NamespaceName,
     pub name: String,
     pub content_type: String,
@@ -799,10 +799,7 @@ impl ContentMetadata {
                 id: value.parent_id,
                 version: 1,
             },
-            root_content_id: ContentMetadataId {
-                id: value.root_content_id,
-                version: 1,
-            },
+            root_content_id: value.root_content_id,
             name: value.file_name,
             content_type: value.mime,
             labels: value.labels,
@@ -825,7 +822,7 @@ impl ContentMetadata {
         indexify_coordinator::ContentMetadata {
             id: value.id.id,
             parent_id: value.parent_id.id,
-            root_content_id: value.root_content_id.id,
+            root_content_id: value.root_content_id,
             file_name: value.name,
             mime: value.content_type,
             labels: value.labels,
@@ -849,7 +846,7 @@ impl Default for ContentMetadata {
                 id: "".to_string(),
                 ..Default::default()
             },
-            root_content_id: ContentMetadataId::default(),
+            root_content_id: ContentMetadataId::default().id,
             namespace: "test_namespace".to_string(),
             name: "test_name".to_string(),
             content_type: "test_content_type".to_string(),
